c0affd65befac70edd6a6466f51c5fd0eaeb9318
jlrandulfe <jlrandulfe@gmail.com>
2017-10-05 11:29:23 +0200

    Correct a bug on the state 6

--- encoder_tick_counter/encoder_tick_counter.ip_user_files/libraries/bldc_decoder.vhd
+++ encoder_tick_counter/encoder_tick_counter.ip_user_files/libraries/bldc_decoder.vhd
@@ -30,7 +30,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity bldc_decoder is
     Port ( clk_200M_in : in std_logic;
-           hall_in : in std_logic_vector (2 downto 0);
+           hall_in : in std_logic_vector (0 downto 2);
            reset_in : in std_logic;
            encoder_pos_out : out std_logic_vector (31 downto 0));
 end bldc_decoder;
@@ -39,6 +39,7 @@ architecture Behavioral of bldc_decoder is
 
 type bldc_state is (state_1, state_2, state_3, state_4, state_5, state_6);
 signal current_state : bldc_state := state_1;
+signal next_state : bldc_state := state_1;
 signal counter_val : std_logic_vector(31 downto 0) := (others => '0');
 
 begin
@@ -57,65 +58,66 @@ begin
 		case current_state is
 			when state_1 =>
 				if (hall_in(1) = '1') then
-					current_state <= state_2;
+					next_state <= state_2;
 					counter_val <= std_logic_vector(unsigned(counter_val)+1); 
 				elsif (hall_in(2) = '1') then
-					current_state <= state_6;
+					next_state <= state_6;
 					counter_val <= std_logic_vector(unsigned(counter_val)-1);
 				else
-					current_state <= state_1;
+					next_state <= state_1;
 				end if;
 			when state_2 =>
 				if (hall_in(0) = '0') then
-					current_state <= state_3;
+					next_state <= state_3;
 					counter_val <= std_logic_vector(unsigned(counter_val)+1);
 				elsif (hall_in(1) = '0') then
-					current_state <= state_1;
+					next_state <= state_1;
 					counter_val <= std_logic_vector(unsigned(counter_val)-1);
 				else
-					current_state <= state_2;
+					next_state <= state_2;
 				end if;
 			when state_3 =>
 				if (hall_in(2) = '1') then
-					current_state <= state_4;
+					next_state <= state_4;
 					counter_val <= std_logic_vector(unsigned(counter_val)+1);
 				elsif (hall_in(0) = '1') then
-					current_state <= state_2;
+					next_state <= state_2;
 					counter_val <= std_logic_vector(unsigned(counter_val)-1);
 				else
-					current_state <= state_3;
+					next_state <= state_3;
 				end if;
 			when state_4 =>
 				if (hall_in(1) = '0') then
-					current_state <= state_5;
+					next_state <= state_5;
 					counter_val <= std_logic_vector(unsigned(counter_val)+1);
 				elsif (hall_in(2) = '0') then
-					current_state <= state_3;
+					next_state <= state_3;
 					counter_val <= std_logic_vector(unsigned(counter_val)-1);
 				else
-					current_state <= state_4;
+					next_state <= state_4;
 				end if;
 			when state_5 =>
 				if (hall_in(0) = '1') then
-					current_state <= state_6;
+					next_state <= state_6;
 					counter_val <= std_logic_vector(unsigned(counter_val)+1);
 				elsif (hall_in(1) = '1') then
-					current_state <= state_4;
+					next_state <= state_4;
 					counter_val <= std_logic_vector(unsigned(counter_val)-1);
 				else
-					current_state <= state_5;
+					next_state <= state_5;
 				end if;
 			when state_6 =>
 				if (hall_in(2) = '0') then
-					current_state <= state_1;
+					next_state <= state_1;
 					counter_val <= std_logic_vector(unsigned(counter_val)+1);
-				elsif (hall_in(1) = '0') then
-					current_state <= state_5;
+				elsif (hall_in(0) = '0') then
+					next_state <= state_5;
 					counter_val <= std_logic_vector(unsigned(counter_val)-1);
 				else
-					current_state <= state_6;
+					next_state <= state_6;
 				end if;
 		end case;
+		current_state <= next_state;
 	end if;
 end process;
 
